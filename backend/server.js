ff8356e1d6d6bf1ed987ea230fd87def33dde095
Pankaj Kumar <pankaj1018+1@gmail.com>
2024-01-18 16:08:53 +0530

    Updated route.rest

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
-const port = 3000
+const port = 3001
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database')) // always use mongod --dbpath /srv/mongodb/
 
@@ -15,13 +15,13 @@ app.use(express.json())
 // Available Routes
 const studentsRouter = require('./routes/students')
 app.use('/students', studentsRouter)
-'localhost:3000/students'
+'localhost:3001/students'
 const seatsRouter = require('./routes/seats')
 app.use('/seats', seatsRouter)
-'localhost:3000/seats'
+'localhost:3001/seats'
 const bookingRouter = require('./routes/bookings')
 app.use('/bookings', bookingRouter)
-'localhost:3000/bookings'
+'localhost:3001/bookings'
 
 
 app.listen(port, () => console.log(`server Started at http://localhost:${port}`));
