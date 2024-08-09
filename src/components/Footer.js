279e6e2551f0e1ffad45b40f276a2bdb312bc6cc
Pankaj Kumar <pankaj1018+1@gmail.com>
2024-08-09 17:02:16 +0530

    Footer layout updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,14 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="py-3 my-4">
+        <footer className="py-2">
             <hr/>
-            <p className="text-center text-body-secondary">© 2024 BookBuddy.co.in</p>
-            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+            <p className="text-center mt-4 text-body-secondary">© 2024 BookBuddy.co.in</p>
+            <ul className="nav justify-content-center mb-2">
                 <li className="nav-item"><a href="https://whatsapp.com/channel/0029VaArd2GDeONE4OiJYn1j" target='_blank' className="nav-link px-2 text-body-secondary">WhatsApp</a></li>
                 <li className="nav-item"><a href="https://t.me/bookbuddylib" target='_blank' className="nav-link px-2 text-body-secondary">Telegram</a></li>
                 <li className="nav-item"><a href="https://www.instagram.com/bookbuddy.co.in/" target='_blank' className="nav-link px-2 text-body-secondary">Instagram</a></li>
-                
             </ul>
-
         </footer>
     )
 }
